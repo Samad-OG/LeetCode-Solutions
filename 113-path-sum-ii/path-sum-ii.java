0fad0b990bd8f25476f394cd5f2c721e4c422348
Samad Adil <samadadil1432005@gmail.com>
2026-09-24 18:44:05 +0530

    Time: 1 ms (99.98%) | Memory: 45.6 MB (38.60%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> result = new ArrayList<>();
+        findPaths(root, targetSum, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void findPaths(TreeNode node, int remainingSum, List<Integer> currentPath, List<List<Integer>> result) {
+        if (node == null) {
+            return;
+        }
+
+        currentPath.add(node.val);
+
+        if (node.left == null && node.right == null && remainingSum == node.val) {
+            result.add(new ArrayList<>(currentPath));
+        } else {
+            findPaths(node.left, remainingSum - node.val, currentPath, result);
+            findPaths(node.right, remainingSum - node.val, currentPath, result);
+        }
+
+        currentPath.remove(currentPath.size() - 1);
+    }
+}
